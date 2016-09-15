da17a6fe83630ef66f8a251e5e31a71f59036529
Giulio Dellorbo <egm0121@gmail.com>
2016-09-15 23:22:06 +0100

    added new api example using diff .pan on diff players

--- ExampleApp/ExampleApp/AudioPlayerView.m
+++ ExampleApp/ExampleApp/AudioPlayerView.m
@@ -1,11 +1,11 @@
 /**********************************************************************************
  AudioPlayer.m
- 
+
  Created by Thong Nguyen on 14/05/2012.
  https://github.com/tumtumtum/audjustable
- 
+
  Copyright (c) 2012 Thong Nguyen (tumtumtum@gmail.com). All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
  1. Redistributions of source code must retain the above copyright
@@ -19,7 +19,7 @@
  4. Neither the name of Thong Nguyen nor the
  names of its contributors may be used to endorse or promote products
  derived from this software without specific prior written permission.
- 
+
  THIS SOFTWARE IS PROVIDED BY Thong Nguyen ''AS IS'' AND ANY
  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -50,18 +50,26 @@
 - (id)initWithFrame:(CGRect)frame andAudioPlayer:(STKAudioPlayer*)audioPlayerIn
 {
     self = [super initWithFrame:frame];
-	
+
     if (self)
 	{
         self.audioPlayer = audioPlayerIn;
-        
+
+        self.vaiPlayer  = [[STKAudioPlayer alloc] initWithOptions:(STKAudioPlayerOptions){ .flushQueueOnSeek = YES, .enableVolumeMixer = YES }];
+        self.vaiPlayer.volume = 1;
+        self.vaiPlayer.pan =   1;
+
+
+        self.secondPlayer  = [[STKAudioPlayer alloc] initWithOptions:(STKAudioPlayerOptions){ .flushQueueOnSeek = YES, .enableVolumeMixer = YES }];
+        self.secondPlayer.volume = 1;
+        self.secondPlayer.pan =   -1;
 		CGSize size = CGSizeMake(220, 50);
-		
+
 		playFromHTTPButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
 		playFromHTTPButton.frame = CGRectMake((frame.size.width - size.width) / 2, frame.size.height * 0.10, size.width, size.height);
 		[playFromHTTPButton addTarget:self action:@selector(playFromHTTPButtonTouched) forControlEvents:UIControlEventTouchUpInside];
 		[playFromHTTPButton setTitle:@"Play from HTTP" forState:UIControlStateNormal];
-        
+
         playFromIcecastButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
         playFromIcecastButton.frame = CGRectMake((frame.size.width - size.width) / 2, frame.size.height * 0.10 + 35, size.width, size.height);
         [playFromIcecastButton addTarget:self action:@selector(playFromIcecasButtonTouched) forControlEvents:UIControlEventTouchUpInside];
@@ -71,58 +79,58 @@
 		playFromLocalFileButton.frame = CGRectMake((frame.size.width - size.width) / 2, frame.size.height * 0.10 + 70, size.width, size.height);
 		[playFromLocalFileButton addTarget:self action:@selector(playFromLocalFileButtonTouched) forControlEvents:UIControlEventTouchUpInside];
 		[playFromLocalFileButton setTitle:@"Play from Local File" forState:UIControlStateNormal];
-        
+
         queueShortFileButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
 		queueShortFileButton.frame = CGRectMake((frame.size.width - size.width) / 2, frame.size.height * 0.10 + 105, size.width, size.height);
 		[queueShortFileButton addTarget:self action:@selector(queueShortFileButtonTouched) forControlEvents:UIControlEventTouchUpInside];
 		[queueShortFileButton setTitle:@"Queue short file" forState:UIControlStateNormal];
-		
+
 		queuePcmWaveFileFromHTTPButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
 		queuePcmWaveFileFromHTTPButton.frame = CGRectMake((frame.size.width - size.width) / 2, frame.size.height * 0.10 + 140, size.width, size.height);
 		[queuePcmWaveFileFromHTTPButton addTarget:self action:@selector(queuePcmWaveFileButtonTouched) forControlEvents:UIControlEventTouchUpInside];
 		[queuePcmWaveFileFromHTTPButton setTitle:@"Queue PCM/WAVE from HTTP" forState:UIControlStateNormal];
-        
+
         size = CGSizeMake(90, 40);
-        
+
 		playButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
 		playButton.frame = CGRectMake(30, 400, size.width, size.height);
 		[playButton addTarget:self action:@selector(playButtonPressed) forControlEvents:UIControlEventTouchUpInside];
-        
+
         stopButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
 		stopButton.frame = CGRectMake((frame.size.width - size.width) - 30, 400, size.width, size.height);
 		[stopButton addTarget:self action:@selector(stopButtonPressed) forControlEvents:UIControlEventTouchUpInside];
         [stopButton setTitle:@"Stop" forState:UIControlStateNormal];
-		
+
 		muteButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
 		muteButton.frame = CGRectMake((frame.size.width - size.width) - 30, 430, size.width, size.height);
 		[muteButton addTarget:self action:@selector(muteButtonPressed) forControlEvents:UIControlEventTouchUpInside];
 		[muteButton setTitle:@"Mute" forState:UIControlStateNormal];
-		
+
 		slider = [[UISlider alloc] initWithFrame:CGRectMake(20, 320, queuePcmWaveFileFromHTTPButton.frame.origin.y + queuePcmWaveFileFromHTTPButton.frame.size.height + 20, 20)];
 		slider.continuous = YES;
 		[slider addTarget:self action:@selector(sliderChanged) forControlEvents:UIControlEventValueChanged];
-        
+
         size = CGSizeMake(80, 50);
-        
+
         repeatSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(30, frame.size.height * 0.15 + 180, size.width, size.height)];
-        
+
         enableEqSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(frame.size.width - size.width - 30, frame.size.height * 0.15 + 180, size.width, size.height)];
         enableEqSwitch.on = audioPlayer.equalizerEnabled;
-        
+
         [enableEqSwitch addTarget:self action:@selector(onEnableEqSwitch) forControlEvents:UIControlEventAllTouchEvents];
 
         label = [[UILabel alloc] initWithFrame:CGRectMake(0, slider.frame.origin.y + slider.frame.size.height + 40, frame.size.width, 25)];
-		
+
         label.textAlignment = NSTextAlignmentCenter;
-        
+
         statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, slider.frame.origin.y + slider.frame.size.height + label.frame.size.height + 50, frame.size.width, 50)];
-		
+
         statusLabel.textAlignment = NSTextAlignmentCenter;
-		
+
 		meter = [[UIView alloc] initWithFrame:CGRectMake(0, 450, 0, 20)];
-		
+
 		meter.backgroundColor = [UIColor greenColor];
-		
+
 		[self addSubview:slider];
 		[self addSubview:playButton];
 		[self addSubview:playFromHTTPButton];
@@ -137,11 +145,11 @@
 		[self addSubview:meter];
 		[self addSubview:muteButton];
         [self addSubview:enableEqSwitch];
-        
+
 		[self setupTimer];
 		[self updateControls];
     }
-	
+
     return self;
 }
 
@@ -156,16 +164,16 @@
 	{
 		return;
 	}
-	
+
 	NSLog(@"Slider Changed: %f", slider.value);
-	
+
 	[audioPlayer seekToTime:slider.value];
 }
 
 -(void) setupTimer
 {
 	timer = [NSTimer timerWithTimeInterval:0.001 target:self selector:@selector(tick) userInfo:nil repeats:YES];
-	
+
 	[[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
 }
 
@@ -176,27 +184,27 @@
 		slider.value = 0;
         label.text = @"";
         statusLabel.text = @"";
-		
+
 		return;
 	}
-	
+
     if (audioPlayer.currentlyPlayingQueueItemId == nil)
     {
         slider.value = 0;
         slider.minimumValue = 0;
         slider.maximumValue = 0;
-        
+
         label.text = @"";
-        
+
         return;
     }
-    
+
     if (audioPlayer.duration != 0)
     {
         slider.minimumValue = 0;
         slider.maximumValue = audioPlayer.duration;
         slider.value = audioPlayer.progress;
-        
+
         label.text = [NSString stringWithFormat:@"%@ - %@", [self formatTimeFromSeconds:audioPlayer.progress], [self formatTimeFromSeconds:audioPlayer.duration]];
     }
     else
@@ -204,20 +212,26 @@
         slider.value = 0;
         slider.minimumValue = 0;
         slider.maximumValue = 0;
-        
+
         label.text =  [NSString stringWithFormat:@"Live stream %@", [self formatTimeFromSeconds:audioPlayer.progress]];
     }
-    
+
     statusLabel.text = audioPlayer.state == STKAudioPlayerStateBuffering ? @"buffering" : @"";
-	
+
 	CGFloat newWidth = 320 * (([audioPlayer averagePowerInDecibelsForChannel:1] + 60) / 60);
-	
+
 	meter.frame = CGRectMake(0, 460, newWidth, 20);
 }
 
 -(void) playFromHTTPButtonTouched
 {
 	[self.delegate audioPlayerViewPlayFromHTTPSelected:self];
+
+  NSURL* secondSongUrl = [NSURL URLWithString:@"http://egm0121-split-cloud.herokuapp.com/asset/kaaris_charge.mp3"];
+  [vaiPlayer playURL:secondSongUrl];
+
+  NSURL* url = [NSURL URLWithString:@"http://egm0121-split-cloud.herokuapp.com/asset/booba_scarface.mp3"];
+  [secondPlayer playURL:url];
 }
 
 -(void) playFromIcecasButtonTouched
@@ -243,7 +257,7 @@
 -(void) muteButtonPressed
 {
 	audioPlayer.muted = !audioPlayer.muted;
-	
+
 	if (audioPlayer.muted)
 	{
 		[muteButton setTitle:@"Unmute" forState:UIControlStateNormal];
@@ -265,7 +279,7 @@
 	{
 		return;
 	}
-    
+
 	if (audioPlayer.state == STKAudioPlayerStatePaused)
 	{
 		[audioPlayer resume];
@@ -278,11 +292,11 @@
 
 -(NSString*) formatTimeFromSeconds:(int)totalSeconds
 {
-    
+
     int seconds = totalSeconds % 60;
     int minutes = (totalSeconds / 60) % 60;
     int hours = totalSeconds / 3600;
-    
+
     return [NSString stringWithFormat:@"%02d:%02d:%02d", hours, minutes, seconds];
 }
 
@@ -304,7 +318,7 @@
 	{
 		[playButton setTitle:@"" forState:UIControlStateNormal];
 	}
-    
+
     [self tick];
 }
 
@@ -317,7 +331,7 @@
 
 	audioPlayer = value;
 	audioPlayer.delegate = self;
-	
+
 	[self updateControls];
 }
 
@@ -339,18 +353,18 @@
 -(void) audioPlayer:(STKAudioPlayer*)audioPlayer didStartPlayingQueueItemId:(NSObject*)queueItemId
 {
 	SampleQueueId* queueId = (SampleQueueId*)queueItemId;
-    
+
     NSLog(@"Started: %@", [queueId.url description]);
-    
+
 	[self updateControls];
 }
 
 -(void) audioPlayer:(STKAudioPlayer*)audioPlayer didFinishBufferingSourceWithQueueItemId:(NSObject*)queueItemId
 {
 	[self updateControls];
-    
+
     // This queues on the currently playing track to be buffered and played immediately after (gapless)
-    
+
     if (repeatSwitch.on)
     {
         SampleQueueId* queueId = (SampleQueueId*)queueItemId;
@@ -364,9 +378,9 @@
 -(void) audioPlayer:(STKAudioPlayer*)audioPlayer didFinishPlayingQueueItemId:(NSObject*)queueItemId withReason:(STKAudioPlayerStopReason)stopReason andProgress:(double)progress andDuration:(double)duration
 {
 	[self updateControls];
- 
+
     SampleQueueId* queueId = (SampleQueueId*)queueItemId;
-    
+
     NSLog(@"Finished: %@", [queueId.url description]);
 }
 
